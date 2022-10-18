348d64a373637aaefcfd3a6c21b3d1a64fe03381
Shantanu788 <114468266+Shantanu788@users.noreply.github.com>
2022-10-18 16:19:26 +0530

    Create Chef And Array Construction.cpp

---
+++ Codechef/Chef And Array Construction.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define int long long
+#define mod 998244353
+using namespace std;
+int n,m,s;
+long long power(long long b,long long n) {
+    long long s=1;
+    while(n) {
+        if(n%2==1)s=s*b%mod;
+        b=b*b%mod;
+        n/=2;
+    }
+    return s;
+}
+long long count(int i,int j) {
+   return ((i/(1<<(j+1)))<<j)+(max(0ll,i%(1<<(j+1))-(1<<j)+1));
+}
+void solve(){
+   cin >> n >> m;s=0;
+   for(int i=0;i<31;++i)
+      s=(s+(power(count(m,i),n)<<i))%mod;
+   cout << s << endl;
+}
+signed main() {
+   int T;cin >> T;
+   while(T--) solve();
+   
+}
