4586bdbd8b800ecaf7926d3e58606a51e22d725d
Shantanu788 <114468266+Shantanu788@users.noreply.github.com>
2022-10-18 16:16:09 +0530

    Create SINGLEOP2.cpp

---
+++ Codechef/SINGLEOP2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Main(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int mAin(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int maIn(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}int maiN(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int MAIN(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int ABCD(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}int efgh(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int heya(){    
+    /*printing information   
+      Multi-Line Comment*/  
+        
+return 0;  
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0;i<n;i++){
+            count++;
+            if(s[i+1]=='1')
+            break;
+            
+        }
+    cout<<count<<endl;
+    }
+    return 0;
+}
